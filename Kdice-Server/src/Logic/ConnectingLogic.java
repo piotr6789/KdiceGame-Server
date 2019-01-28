ffc9b4a63c557462006411189ce5a3f8402c7505
Piotr Ulanicki <piotr.ulanicki4@gmail.com>
2019-01-28 22:08:28 +0100

    10 games work, still need results

--- Kdice-Server/src/Logic/ConnectingLogic.java
+++ Kdice-Server/src/Logic/ConnectingLogic.java
@@ -2,6 +2,7 @@ package Logic;
 
 import Models.BoardModel;
 import Models.PlayerModel;
+import org.omg.PortableServer.THREAD_POLICY_ID;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -17,11 +18,13 @@ public class ConnectingLogic
     public static int counter = 0;
     public static BoardModel board;
     public static int roundCounter = 0;
+    public static int gameCounter = 0;
     public static String answerClient = "";
 
     public static void startConnect(ServerSocket serverSocket) throws IOException, InterruptedException {
         int id = 1;
 
+        try {
             while (counter < 1) {
                 counter++;
                 if (playerList.size() < 5) {
@@ -52,44 +55,64 @@ public class ConnectingLogic
                     }
                 }
 
-                board = new BoardModel(playerList);
-                board.createBoard();
-                board.setUpPlayersAndCubes();
 
-                while (roundCounter < 10) {
-                    board.printBoard();
-                    GameLogic.cleanBuffor(playerList);
-                    for (PlayerModel player : playerList) {
-                        player.get_outClient().writeUTF("TWOJ RUCH");
+                while (gameCounter < 10) {
+                    gameCounter++;
+
+                    roundCounter = 0;
+                    board = new BoardModel(playerList);
+                    board.createBoard();
+                    board.setUpPlayersAndCubes();
+
+
+                    Thread.sleep(2000);
+                    while (roundCounter < 100) {
+
+                        for (PlayerModel player : playerList) {
+                            player.get_outClient().writeUTF("START GRY");
+                        }
                         board.printBoard();
-                        answerClient = player.get_inClient().readUTF();
-                        while (answerClient.substring(0, 4).equals("ATAK")) {
-                            player.get_outClient().writeUTF("OK");
-                            for (PlayerModel players : playerList) {
-                                if (players.get_id() != player.get_id()) {
-                                    players.get_outClient().writeUTF(answerClient);
-                                }
-                            }
-                            board.updateBoardAfterAttack(answerClient, player.get_id());
+                        GameLogic.cleanBuffor(playerList);
+
+
+                        for (PlayerModel player : playerList) {
+                            player.get_outClient().writeUTF("TWOJ RUCH");
                             board.printBoard();
-                            GameLogic.cleanBuffor(playerList);
                             answerClient = player.get_inClient().readUTF();
+                            while (answerClient.substring(0, 4).equals("ATAK")) {
+                                player.get_outClient().writeUTF("OK");
+                                for (PlayerModel players : playerList) {
+                                    if (players.get_id() != player.get_id()) {
+                                        players.get_outClient().writeUTF(answerClient);
+                                    }
+                                }
+                                board.updateBoardAfterAttack(answerClient, player.get_id());
+                                board.printBoard();
+                                GameLogic.cleanBuffor(playerList);
+                                answerClient = player.get_inClient().readUTF();
+                            }
+                            if (answerClient.equals("PASS")) {
+                                player.get_outClient().writeUTF("PASS");
+                                continue;
+                            }
                         }
-                        if (answerClient.equals("PASS")) {
-                            player.get_outClient().writeUTF("PASS");
-                            continue;
-                        }
+
+                            for (PlayerModel player : playerList) {
+                                player.get_outClient().writeUTF("KONIEC RUNDY");
+                            }
+
+                        board.addCubesAfterRound();
+//                    Thread.sleep(1000);
+                        roundCounter++;
                     }
-
-
-                    for (PlayerModel player : playerList) {
-                        player.get_outClient().writeUTF("KONIEC RUNDY");
-                    }
-
-                    board.addCubesAfterRound();
-                    Thread.sleep(1000);
-                    roundCounter++;
                 }
+
             }
+            for (PlayerModel player : playerList) {
+                player.get_outClient().writeUTF("KONIEC GRY");
+            }
+        }catch(Exception io){
+            io.printStackTrace();
+        }
     }
 }
